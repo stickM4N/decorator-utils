0b698d675f12a079da6e330d7a35748e714ccb6a
Julio C. Galindo <julio.galindo@bionet.com>
2024-04-13 20:32:40 +0200

    created decorator context class

---
+++ decorator_utils/context.py
@@ -0,0 +1,43 @@
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+from typing import Any, final, Tuple
+
+from decorator_utils import function_wrapper, FunctionMetadata
+
+
+class DecoratorContext(ABC):
+    """
+    Decorator class that provides an interface to implement a decorator with the ability to retain data between
+    `pre_cb` and `post_cb` callbacks. It works with `function_wrapper` in the back and its parameters are matched
+    against this class attributes.
+    """
+    _use_result: bool = True
+    ''' Whether to pass decorated function result to `post_func` as first arg. '''
+    _force_args: bool = True
+    ''' Whether to force `args` or `kwargs` to be passed tho callback functions. '''
+    _forward_metadata: bool = False
+    ''' Whether to add `_func_metadata` to `kwargs`. '''
+    _return_metadata: bool = False
+    ''' Whether to also return `_func_metadata` as result in a tuple (`result`, `metadata`). '''
+
+    @final
+    def __init__(self, func):
+        self._decorated_function = func
+
+    @final
+    def __call__(self, *args, **kwargs) -> Any | Tuple[Any, FunctionMetadata]:
+        decorator = function_wrapper(self.pre_cb, self.post_cb,
+                                     use_result=self._use_result, force_args=self._force_args,
+                                     forward_metadata=self._forward_metadata, return_metadata=self._return_metadata)
+        callback = decorator(self._decorated_function)
+
+        return callback(*args, **kwargs)
+
+    @abstractmethod
+    def pre_cb(self, *args, **kwargs) -> None:
+        raise NotImplementedError
+
+    @abstractmethod
+    def post_cb(self, *args, **kwargs) -> None:
+        raise NotImplementedError
