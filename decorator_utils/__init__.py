fc605b9fae8b13feae5c5d6795191b6c85223e44
Julio C. Galindo <julio.galindo@bionet.com>
2024-04-14 09:04:12 +0200

    created benchmark decorator

--- decorator_utils/__init__.py
+++ decorator_utils/__init__.py
@@ -1,6 +1,7 @@
 from .context import DecoratorContext
+from .decorators.benchmark import Benchmark
 from .metadata import FunctionMetadata
 from .wrapper import function_wrapper
 
 
-__version__ = '1.1.0'
+__version__ = '1.1.1'
